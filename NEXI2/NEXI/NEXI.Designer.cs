23807bb5dcc89b597968b9ed6dbb2ed39828c22d
Talal Hassan <reality101failure@gmail.com>
2018-05-28 09:40:41 +0500

    Nexi 2

---
+++ NEXI2/NEXI/NEXI.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace NEXI
+{
+    partial class NEXI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NEXI));
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.TitleIcon = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyNEXInotifier = new System.Windows.Forms.NotifyIcon(this.components);
+            this.commandBox = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Red;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnClose.Location = new System.Drawing.Point(586, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(34, 27);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackColor = System.Drawing.Color.Gold;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnMinimize.Location = new System.Drawing.Point(546, 5);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(34, 27);
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.Text = "-";
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // TitleIcon
+            // 
+            this.TitleIcon.BackColor = System.Drawing.Color.Transparent;
+            this.TitleIcon.FlatAppearance.BorderSize = 0;
+            this.TitleIcon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TitleIcon.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleIcon.ForeColor = System.Drawing.Color.Gold;
+            this.TitleIcon.Image = ((System.Drawing.Image)(resources.GetObject("TitleIcon.Image")));
+            this.TitleIcon.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.TitleIcon.Location = new System.Drawing.Point(12, 5);
+            this.TitleIcon.Name = "TitleIcon";
+            this.TitleIcon.Size = new System.Drawing.Size(101, 53);
+            this.TitleIcon.TabIndex = 3;
+            this.TitleIcon.Text = "EXI";
+            this.TitleIcon.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.TitleIcon.UseVisualStyleBackColor = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Gold;
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStart.Location = new System.Drawing.Point(172, 383);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(129, 38);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.FlatAppearance.BorderSize = 0;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStop.Location = new System.Drawing.Point(331, 383);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(129, 38);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyNEXInotifier
+            // 
+            this.notifyNEXInotifier.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyNEXInotifier.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyNEXInotifier.Icon")));
+            this.notifyNEXInotifier.Text = "Settings";
+            this.notifyNEXInotifier.Visible = true;
+            this.notifyNEXInotifier.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyNEXInotifier_MouseDoubleClick);
+            // 
+            // commandBox
+            // 
+            this.commandBox.Location = new System.Drawing.Point(19, 54);
+            this.commandBox.Multiline = true;
+            this.commandBox.Name = "commandBox";
+            this.commandBox.Size = new System.Drawing.Size(601, 283);
+            this.commandBox.TabIndex = 7;
+            // 
+            // NEXI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(625, 448);
+            this.Controls.Add(this.commandBox);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.TitleIcon);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.btnClose);
+            this.ForeColor = System.Drawing.Color.DarkOrange;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NEXI";
+            this.Text = "NEXI";
+            this.Move += new System.EventHandler(this.NEXI_Move);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button TitleIcon;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyNEXInotifier;
+        private System.Windows.Forms.TextBox commandBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ NEXI2/NEXI/NEXI.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+
+namespace NEXI
+{
+    public partial class NEXI : Form
+    {
+        SpeechSynthesizer nexi = new SpeechSynthesizer();
+        PromptBuilder pb = new PromptBuilder();
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        Choices cList = new Choices();
+        Process[] process;
+        string[] CommandsForNexi = File.ReadAllLines(Environment.CurrentDirectory + "\\CommandsForNexi.txt");
+        string[] KeywordsOfCommands = File.ReadAllLines(Environment.CurrentDirectory + "\\KeywordsOfCommands.txt");
+        //string[] commands ;
+       
+             
+
+
+        //NotifyIcon icon = new NotifyIcon();
+        public NEXI()
+        {
+            InitializeComponent();
+
+            //btnStart.PerformClick();
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            
+            ////creating new doc
+            //cList.Add("exit sticky note", "take a shot", "exit chrome", "exit word", "nexi", "hi nexi", "hello", "open word", "open power point", "how are you", "what is the current time", "open sticky note", "open chrome", "open visual studio", "thank you", "close", "exit power point", "search");
+            //Grammar gr = new Grammar(new GrammarBuilder(cList));
+            cList.Add(CommandsForNexi);
+            Grammar gr = new Grammar(new GrammarBuilder(cList));
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            if (WindowState == FormWindowState.Normal)
+                WindowState = FormWindowState.Minimized;
+            this.Hide();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            sre.RecognizeAsyncStop();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //btnStart.Enabled = false;
+            //btnStop.Enabled = true;
+            //////creating new doc
+            ////cList.Add("exit sticky note", "take a shot", "exit chrome", "exit word", "nexi", "hi nexi", "hello", "open word", "open power point", "how are you", "what is the current time", "open sticky note", "open chrome", "open visual studio", "thank you", "close", "exit power point", "search");
+            ////Grammar gr = new Grammar(new GrammarBuilder(cList));
+            //cList.Add(CommandsForNexi);
+            //Grammar gr = new Grammar(new GrammarBuilder(cList));
+
+            //try
+            //{
+            //    sre.RequestRecognizerUpdate();
+            //    sre.LoadGrammar(gr);
+            //    sre.SpeechRecognized += sre_SpeechRecognized;
+            //    sre.SetInputToDefaultAudioDevice();
+            //    sre.RecognizeAsync(RecognizeMode.Multiple);
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Error");
+            //}
+        }
+        public void captureIt()
+        {
+            Bitmap capture = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(capture as Image);
+            g.CopyFromScreen(0, 0, 0, 0, capture.Size);
+            capture.Save("image.jpeg", ImageFormat.Jpeg);
+        }
+        private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //string speechWeb = e.Result.Text;
+            switch (e.Result.Text.ToString())//isko comment krna hai ammendment ke liye
+            {
+                //try
+                //{
+
+                //    sreWeb.LoadGrammar(grWeb);
+                //    try
+                //    {
+                //        webSearchCommands = File.ReadAllLines(Environment.CurrentDirectory + "\\WebSearchCommands.txt");
+                //        webSearchKeywords = File.ReadAllLines(Environment.CurrentDirectory + "\\WebSearchKeywords.txt");
+                //        Grammar webSearchCommandGrammar = new Grammar(new GrammarBuilder(new Choices(webSearchCommands)));
+                //        sreWeb.LoadGrammarAsync(webSearchCommandGrammar);
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        nexiWeb.SpeakAsync("I have detected an in valid entry in your web commands" + ex.Message);
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //    MessageBox.Show(ex.Message);
+                //} isko sae kr ke chalana hai
+
+                //case "nexi":
+                //{
+                //    notifyNEXInotifier.ShowBalloonTip(1000, "NEXI Running", "Listening to your command", ToolTipIcon.Info);
+                //    string command = e.Result.ToString(); int c = 0;
+                //    try
+                //    {
+                //        foreach (string line in CommandsForNexi)
+                //        {
+                //            if (line == command)
+                //            {
+
+                //                nexi.SpeakAsync("Opening" + KeywordsOfCommands[c]);
+                //                Process.Start(KeywordsOfCommands[c]);
+
+                //            }
+                //            c += 1;
+                //        }
+                //    }
+                //    catch(Exception ex)
+                //    {
+                //        c += 1;
+                //        nexi.SpeakAsync("Please check the " + command + "command on line " + c + ". It appears to be missing a proper response." + ex.Message);
+                //    }
+                //    break;
+                //}
+                //default:
+                //    {
+
+                //    }
+
+
+
+                case "hello":
+                case "hi nexi":
+                case "nexi":
+                    nexi.SpeakAsync("hello Talal");
+                    break;
+                case "how are you":
+                    nexi.SpeakAsync("i am good. Thanks for asking");
+                    break;
+                case "what is the current time":
+                    nexi.SpeakAsync("Current time is" + DateTime.Now.ToLongTimeString());
+                    break;
+                case "open chrome":
+                    nexi.SpeakAsync("Opening Chrome"); Process.Start("chrome", "https://www.google.com");
+                    break;
+                case "thank you":
+                    nexi.SpeakAsync("pleasure is mine Talal");
+                    break;
+                case "close":
+                    nexi.SpeakAsync("closing"); Application.Exit();
+                    break;
+                case "open sticky note":
+                    nexi.SpeakAsync("Opening sticky note"); System.Diagnostics.Process.Start(@"C:\\Windows\\Sysnative\\StikyNot.exe");
+                    break;
+                case "open word":
+                    nexi.SpeakAsync("opening microsoft word"); Process.Start("winword");
+                    break;
+                case "open power point":
+                    nexi.SpeakAsync("opening microsoft powerpoint"); Process.Start("powerpnt");
+                    break;
+                case "open visual studio":
+                    nexi.SpeakAsync("opening visual studio"); Process.Start("denev");
+                    break;
+                case "exit word":
+                    {
+                        nexi.SpeakAsync("closing word");
+                        //Process.Kill("winword");
+                        process = Process.GetProcessesByName("winword");
+                        foreach (Process w in process)
+                        {
+                            w.Kill();
+                        }
+                        break;
+                    }
+                case "exit sticky note":
+                    {
+                        nexi.SpeakAsync("closing sticky note");
+                        process = Process.GetProcessesByName(@"C:\\Windows\\Sysnative\\StikyNot.exe");
+                        foreach (Process s in process)
+                        {
+                            s.Kill();
+                        }
+                        break;
+                    }
+                case "exit chrome":
+                    {
+                        nexi.SpeakAsync("closing chrome");
+                        process = Process.GetProcessesByName("chrome");
+                        foreach (Process c in process)
+                        {
+                            c.Kill();
+                        }
+                        break;
+
+                    }
+                case "take a shot":
+                    {
+                        nexi.SpeakAsync("taking shot");
+                        captureIt();
+                        break;
+                    }
+                case "lock computer":
+                    System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+                    break;
+                case "search":
+                    //this.Hide();
+                    sre.RecognizeAsyncStop();
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = false;
+                    WebSearch web = new WebSearch();
+                    web.Show();
+
+                    break;
+                //default:
+                //    sre.RequestRecognizerUpdate();
+                //    //    sre.LoadGrammar(gr);
+                //    sre.SpeechRecognized += sre_SpeechRecognized;
+                //    sre.SetInputToDefaultAudioDevice();
+                //    sre.RecognizeAsync(RecognizeMode.Multiple);
+                //    //this.Show();
+                    
+                //    btnStart.Enabled = false;
+                //    btnStop.Enabled = true;
+                //    break;
+
+                    //    foreach (RecognizedPhrase phrase in e.Result.Alternates)
+                    //    {
+                    //        nexi.SpeakAsync("Rejected phrase" + phrase.Text);
+                    //    }
+                    //    //nexi.SpeakAsync("Cannot understand that. Do you want me to add this in my data set");
+                    //    break;
+
+
+            }
+            commandBox.Text += e.Result.Text.ToString() + Environment.NewLine;
+
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NEXI_Move(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+
+                notifyNEXInotifier.ShowBalloonTip(1000, "NEXI Running", "Say HI NEXI to activate", ToolTipIcon.Info);
+                this.Hide();
+            }
+        }
+
+        private void notifyNEXInotifier_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+                WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
